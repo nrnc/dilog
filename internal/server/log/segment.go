19e5116ca67ebc258f06fb1e38d763a55a695ea6
nchukkaio <narendra.chukka@yahoo.in>
2021-09-11 01:21:06 +0530

    implemented log and unit tests for log

--- internal/server/log/segment.go
+++ internal/server/log/segment.go
@@ -117,9 +117,5 @@ func (s *segment) Remove() error {
 }
 
 func nearestMultiple(j, k uint64) uint64 {
-	if j >= 0 {
-		return (j / k) * k
-	}
-	return ((j - k + 1) / k) * k
-
+	return (j / k) * k
 }
